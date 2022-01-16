86063a47eccb4fc919e806114cbc5de3d3d2d1d7
tora <ermekovtariel@gmail.com>
2022-01-16 14:26:37 +0600

    initialization mobile app

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
+import { Navbar } from './src/Components/Navbar';
+
+import { app } from './src/AppStyles';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={app.container}>
+      <Navbar />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/AppStyles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export const app = StyleSheet.create({
+  container: {
+    flex: 1,
+    textAlign: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export const navBar = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    textAlign: 'center',
+    justifyContent: 'center',
+  },
+});
